8f451c9ffe62b385564642b8c2b9e541745f3aea
Esther Kang <estherkang14@gmail.com>
2020-05-21 22:31:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,8 +24,8 @@ def get_japanese_emoticon(usable_emoticons, emoticon)
   load_lib_emo = load_library(usable_emoticons)
   #binding.pry 
   
-  emoji = emoticons.values.find {|faces| faces[0] == emoticon}
-  emoji == nil ? "Sorry, that emoticon was not found" : emoji[1]
+  correct_emoji = emoticons.values.find {|faces| faces[0] == emoticon}
+  emoji == nil ? "Sorry, that emoticon was not found" : correct_emoji[1]
   
   #binding.pry
  # load_lib_emo.each  do |name_of_emoji, hash_of_name|
