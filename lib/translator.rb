bbf8f9d97b00dd6c87533df41ce302a89e35fc37
Esther Kang <estherkang14@gmail.com>
2020-05-21 22:15:49 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,11 +24,10 @@ def get_japanese_emoticon(usable_emoticons, emoticon)
   load_lib_emo = load_library(usable_emoticons)
   #binding.pry 
   
-  binding.pry 
   load_lib_emo.each do |name_of_emoji, hash_of_name|
+  binding.pry
+
   
-   
-  end
 
   end
   
