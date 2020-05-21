acff43cc13e50bcb62293cfb647179f19feb9a23
Esther Kang <estherkang14@gmail.com>
2020-05-21 19:18:01 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,9 +1,8 @@
 # require modules here
 require 'pry'
-require 'yaml'
+require "yaml"
 usable_emoticons = YAML.load_file('emoticons.yml')
-usable_emoticons 
-binding.pry 
+
 
 def load_library
   # code goes here
