d6be00927cb15a87724ec3dd0cc073d605d6edec
Esther Kang <estherkang14@gmail.com>
2020-05-21 20:19:18 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ def load_library(usable_emoticons)
 usable_emoticons_lib = {'meaning' => {}, 'emoji' => {} }
 
 usable_emoticons.each do |name, translated| 
-  usable_emoticons_lib['meaning'][translated]
+  usable_emoticons_lib['meaning'][translated[1]]
  
   
 end
