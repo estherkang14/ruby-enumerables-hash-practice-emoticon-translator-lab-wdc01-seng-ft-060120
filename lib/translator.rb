80ebc00367d0a1f47e3d0149d9a079e03d9a9967
Esther Kang <estherkang14@gmail.com>
2020-05-22 00:32:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -9,7 +9,7 @@ def load_library(usable_emoticons)
   usable_emoticons = YAML.load_file('lib/emoticons.yml')
 
 usable_emoticons_lib = usable_emoticons.each_with_object({}) do |(key, value), main_hash|
-  binding.pry 
+  #binding.pry 
   if !main_hash[key] 
     eng_emo = usable_emoticons[key][0]
     jap_emo = usable_emoticons[key][1]
